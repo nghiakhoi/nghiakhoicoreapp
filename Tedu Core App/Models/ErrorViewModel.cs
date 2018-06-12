a0abc7db8cf671328803b90bf54a6e8748033bbf
nghiakhoi <hnghiakhoi3@gmail.com>
2018-06-13 00:07:03 +0700

    Add project files.

---
+++ Tedu Core App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tedu_Core_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
